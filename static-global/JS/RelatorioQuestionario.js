876ec4ec19312b23213fd2b4c19ece2d1d1ab01a
Anderson Freitas <freitas.dev@proton.me>
2024-06-07 01:59:56 -0300

    update finish relatorio

--- static-global/JS/RelatorioQuestionario.js
+++ static-global/JS/RelatorioQuestionario.js
@@ -17,6 +17,7 @@ function insertRelatorio(data){
 
     // console.log(`Index: ${Object.getOwnPropertyNames(data)}`);
     // console.log(`Dados: ${data}`)
+    console.log(data)
 
     var container = $('#containerRelatorio')
 
@@ -32,7 +33,8 @@ function insertRelatorio(data){
         <div id="containerPerguntas">
         </div>
     `)
-
+    perguntasAssociativas = data.perguntas.filter(pergunta => pergunta.tipo === 3)
+    
     data.perguntas.forEach(pergunta => {
         if(pergunta.ativo){
 
@@ -188,28 +190,100 @@ function insertRelatorio(data){
                     `)
                     break;
                 case 3:
+                    var canvasWidth = (1068 / 100) * (100/pergunta.alternativas.length);
+
+                    // console.log(canvasWidth)
+
                     $('#containerPerguntas').append(`
                         <div id="container-Pergunta-${pergunta.id}">
                             <h1 class="fs-5 text-info text-center">
                                 ${pergunta.descricao}
                             </h1>
+                            <div id="container-canvas-${pergunta.id}" class="d-flex justify-content-between" style="width:100%;">
+                                
+                            </div>
                         </div>
                     `)
 
-                    $(`#container-Pergunta-${pergunta.id}`).append(`
-                        <h4 class="fs-6 text-secondary text-center">
-                            Disposição das respostas:
-                        </h4>
-                        <div class="d-flex flex-column">
-                            ${ pergunta.respostas.map(function(a){
-                                if(a.valor){
-                                    return `<div class="mx-1 text-secondary"><b>${a.quemRespondeu.nome} ${a.quemRespondeu.sobrenome}:</b> ${(a.valor)}</div>`
+                    pergunta.associacoes.forEach((a) => {
+                      $(`#container-canvas-${pergunta.id}`).append(
+                        `<canvas id="canvas-${a.id}" width="${canvasWidth}" height="534"></canvas>`
+                      )
+                    })
+
+                    // console.log(pergunta)
+
+                    // console.log(pergunta.alternativas.length)
+                    // console.log(pergunta.associacoes)
+                    
+                    var dict = {}
+                    var listData = []
+                    var labelAcept = true
+
+                    pergunta.respostas.forEach((resposta, index) =>{
+                        pergunta.associacoes.forEach((l) => {
+                            dict[l.id] = {}
+                            pergunta.alternativas.forEach((l2) =>{
+                                dict[l.id][l2.id] = 0
+                            })
+                        })
+                    })
+
+                    pergunta.respostas.forEach((resposta, index) =>{            
+                        rS = resposta.valor.split(',')
+                        rS.forEach((r)=>{
+                            if(r){
+                                rdp = r.split(':')
+                                dict[rdp[1]][rdp[0]]++
+                            }
+                        })
+
+                    })
+
+                    console.log(dict)
+
+                    pergunta.associacoes.map(function(e){
+                        listData = []
+                        pergunta.alternativas.map(function(a){
+                            listData.push({
+                                label: a.valor,
+                                data:[dict[e.id][a.id]]
+                            })
+                        })
+                        console.log(e.valor,listData)
+
+                        e.valor.length > 50 ? labelAcept = false : labelAcept = true
+
+                        var data = {
+                            labels:[e.valor.length > 50 ? e.valor.substr(0, 50) + '...': e.valor],
+                            datasets:listData
+                        }
+
+                        new Chart($(`#canvas-${e.id}`), {
+                            type: 'bar',
+                            data: data,
+                            options: {
+                                responsive: false,
+                                maintainAspectRatio: false,
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
+                                    },
                                 }
-                            }).join(' ')}
-                        </div>
+                            }
+                        })
+
+                    })
+
+                    $(`#container-Pergunta-${pergunta.id}`).append(`
                         <hr>
+                        ${labelAcept ? '' : pergunta.associacoes.map(function(a,index){
+                            return `<p>${index+1}) ${a.valor}</p>`
+                        }).join(' ')}
                     `)
+
                     break;
+
                 
                 
             }
